476a3c25caf7d445a7d35464bf4203cdbae3c1ed
Rugved2207 <rugvedkhot123456@gmail.com>
2026-05-22 20:06:55 +0000

    Some extra additions

--- src/Ch01/01_02e/CodeDemo.cpp
+++ src/Ch01/01_02e/CodeDemo.cpp
@@ -4,9 +4,20 @@
 
 #include <iostream>
 
-int main(){
+/* This is used to add comments to your code*/
+
+int main(){ //This is the main function
     std::cout << "Hi There!" << std::endl;
 
     std::cout << std::endl << std::endl;
+    return 0;
+}
+
+// Another way of writing the same function using cstdio
+
+#include <cstdio>
+
+int main2(){
+    printf("Hello world a second time");
     return 0;
 }
