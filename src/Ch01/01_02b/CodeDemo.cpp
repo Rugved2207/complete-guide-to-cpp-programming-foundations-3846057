9d2723a8f23e34e69483b052c1b171948d762454
Rugved2207 <rugvedkhot123456@gmail.com>
2026-05-22 20:06:41 +0000

    I edited it

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,25 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+
+#include <iostream>
+
+/* This is used to add comments to your code*/
+
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+    std::cout << std::endl << std::endl;
+    return 0;
+}
+
+// Another way of writing the same function using cstdio
+
+#include <cstdio>
+
+int main2(){
+    printf("Hello world a second time \n");
+    return 0;
+}
+
