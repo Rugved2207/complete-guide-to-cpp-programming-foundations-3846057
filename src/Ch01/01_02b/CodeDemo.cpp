2be43f59802a1291059d14b2108f44c34e97becb
Rugved2207 <rugvedkhot123456@gmail.com>
2026-05-27 18:52:56 +0000

    notes

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -7,13 +7,6 @@
 
 /* This is used to add comments to your code*/
 
-
-int main(){
-    std::cout << "Hello World" << std::endl;
-    std::cout << std::endl << std::endl;
-    return 0;
-}
-
 // Another way of writing the same function using cstdio
 
 #include <cstdio>
@@ -22,4 +15,14 @@ int main2(){
     printf("Hello world a second time \n");
     return 0;
 }
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+    main2();
+    std::cout << std::endl << std::endl;
+    return 0;
+}
+
+/* Having statements or expressions such as "a = 5" sets the variable a to the value of 5. This needs to be inside of the function*/
+
 
