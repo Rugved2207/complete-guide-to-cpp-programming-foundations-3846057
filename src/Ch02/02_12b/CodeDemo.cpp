744161790922ef29c51c6b7e0b1a58cbaab7a73b
Rugved2207 <rugvedkhot123456@gmail.com>
2026-06-06 22:07:34 +0000

    modified

--- src/Ch02/02_12b/CodeDemo.cpp
+++ src/Ch02/02_12b/CodeDemo.cpp
@@ -5,6 +5,14 @@
 #include <iostream>
 #include <typeinfo>
 
+int add_int(float a, double b, long double c){
+    int result = 0;
+    
+    std::cout << "The result is" << static_cast<int>(a) + static_cast<int>(b) + static_cast<int>(c) << std::endl;
+
+    return result;
+}
+
 int main(){
     auto score = 8;
     auto points = 12345678901;
@@ -22,6 +30,9 @@ int main(){
     std::cout << "The type of initial is " << typeid(initial).name() << std::endl;
     std::cout << "The type of title is " << typeid(title).name() << std::endl;
 
+    add_int(2.1, 3.9, 4.6);
     std::cout << std::endl << std::endl;
     return 0;
 }
+
+
